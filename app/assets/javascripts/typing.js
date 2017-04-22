f0a9843b3e8d2fb4eb502fbfe2228e11c2b1742e
Eben <eben.lowe@gmail.com>
2017-04-22 14:38:26 -0400

    Add additional channel broadcast parameter ('clear' boolean.)

--- app/assets/javascripts/typing.js
+++ app/assets/javascripts/typing.js
@@ -2,12 +2,19 @@ App.room = App.cable.subscriptions.create("WebNotificationsChannel", {
   received: function(data) {
     var messagesDiv, messageElement, textNode;
     messagesDiv = document.getElementById("messages");
-    if (messagesDiv.firstChild) {
-      messagesDiv.removeChild(messagesDiv.firstChild)
+    if (messagesDiv.firstChild && data.clear) {
+      while (messagesDiv.firstChild) {
+      messagesDiv.removeChild(messagesDiv.firstChild);
+      }
+    } else {
+      var messagesDivChildren = messagesDiv.children;
+      for (var i = 0, length = messagesDivChildren.length; i < length; i++) {
+        messagesDivChildren[i].classList.remove("cursor");
+      }
     }
     messageElement = document.createElement("h2");
-    messageElement.classList.add("text-center");
-    textNode = document.createTextNode(data["message"]);
+    messageElement.classList.add("cursor");
+    textNode = document.createTextNode(data.message);
     messageElement.appendChild(textNode);
     return messagesDiv.appendChild(messageElement);
   }
